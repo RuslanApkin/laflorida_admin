107ed262e877cd69a58e482e48fa1343a146e65d
RuslanApkin <ruslan-apkin@yandex.ru>
2022-03-25 19:43:11 +0300

    smoll dark

--- client/src/Pages/CategoryEdit/CategoryEdit.jsx
+++ client/src/Pages/CategoryEdit/CategoryEdit.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CategoryDnD from "./CategoryDnD";
 
 export default function CategoryEdit() {
   return (
     <>
       <h2 className="mb-4 text-xl">Редактор категорий товаров</h2>
+      <CategoryDnD />
     </>
   );
 }
